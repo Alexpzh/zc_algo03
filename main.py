211dccc9197a1ea8b1f35e188c973dfa128e03dc
Alexpzh <166602027+Alexpzh@users.noreply.github.com>
2024-08-09 18:06:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -99,6 +99,20 @@ print('Разница между сортировками: ')
 print (b-bb)
 
 #   Сортировка слиянием (Merge Sort)
+def merge_list(left, right):
+    result = []
+    i, j = 0, 0
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    result += left[i:]
+    result += right[j:]
+    return result
+
 def merge_sort(arr):
     if len(arr) > 1:
         r = len(arr) // 2

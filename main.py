1c0472854f04e5ac39b808bf4d90ccf6569e3012
Alexpzh <166602027+Alexpzh@users.noreply.github.com>
2024-08-09 18:01:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ import numpy as np
 import timeit
 
 a = np.random.randint(-100000, 100000, 10000)
+#a = [-3, 5, 0, -8, 1, 10]
+print("\nИсходный массив: ")
 print(a)
+bb = a.copy()
+bb.sort()
 
 #   Пузырьковая сортировка (Bubble Sort);
 def bubble_sort(a):
@@ -20,8 +24,10 @@ tt0 = time.time()
 bubble_sort(b)
 tt1 = time.time()
 
-print('Сортировка пузырьком: ', format(tt1 - tt0, '.4f'))
+print('\nСортировка пузырьком: ', format(tt1 - tt0, '.4f'))
 print(b)
+print('Разница между сортировками: ')
+print (b-bb)
 
 
 
@@ -41,8 +47,10 @@ tt0 = time.time()
 selection_sort(b)
 tt1 = time.time()
 
-print('Сортировка выбором: ', format(tt1 - tt0, '.4f'))
+print('\nСортировка выбором: ', format(tt1 - tt0, '.4f'))
 print(b)
+print('Разница между сортировками: ')
+print (b-bb)
 
 #   Быстрая сортировка (Quick Sort);
 def quick_sort(s):
@@ -59,11 +67,13 @@ b = []
 b = a.copy()
 
 tt0 = time.time()
-quick_sort(b)
+b = quick_sort(b)
 tt1 = time.time()
 
-print('Быстрая сортировка: ', format(tt1 - tt0, '.4f'))
-print(b)
+print('\nБыстрая сортировка: ', format(tt1 - tt0, '.4f'))
+#print(b)
+print('Разница между сортировками: ')
+print (b-bb)
 
 
 
@@ -80,13 +90,53 @@ b = []
 b = a.copy()
 
 tt0 = time.time()
-quick_sort2(b)
+b = quick_sort2(b)
 tt1 = time.time()
 
-print('Быстрая сортировка (list): ', format(tt1 - tt0, '.4f'))
-print(b)
+print('\nБыстрая сортировка (list): ', format(tt1 - tt0, '.4f'))
+#print(b)
+print('Разница между сортировками: ')
+print (b-bb)
 
 #   Сортировка слиянием (Merge Sort)
+def merge_sort(arr):
+    if len(arr) > 1:
+        r = len(arr) // 2
+        left = arr[:r]
+        right = arr[r:]
+
+        merge_sort(left)
+        merge_sort(right)
+
+        i = j = k = 0
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                arr[k] = left[i]
+                i += 1
+            else:
+                arr[k] = right[j]
+                j += 1
+            k += 1
+
+        while i < len(left):
+            arr[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            arr[k] = right[j]
+            j += 1
+            k += 1
+
+b = a.copy()
+tt0 = time.time()
+merge_sort(b)
+tt1 = time.time()
+
+print('\nСортировка слиянием: ', format(tt1 - tt0, '.4f'))
+print(b)
+print('Разница между сортировками: ')
+print (b-bb)
 
 #inp = input("Enter number: ")
 
@@ -103,21 +153,15 @@ def insert_sort(arr):
         arr[j + 1] = key
 
 
-b = []
 b = a.copy()
-
 tt0 = time.time()
 insert_sort(b)
 tt1 = time.time()
 
-print('Сортировка вставками: ', format(tt1 - tt0, '.4f'))
+print('\nСортировка вставками: ', format(tt1 - tt0, '.4f'))
 print(b)
 # Результат: [-8, -3, 0, 1, 5, 10]
 
 print('Разница между сортировками: ')
-bb = []
-bb = a.copy()
-bb.sort()
-
 print (b-bb)
 

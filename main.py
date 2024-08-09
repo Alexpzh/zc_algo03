dc664691bbb276d0d86c0a186485f7dc78456da3
Alexpzh <166602027+Alexpzh@users.noreply.github.com>
2024-08-09 15:38:17 +0300

    Add quick_sort

--- main.py
+++ main.py
@@ -1,21 +1,39 @@
 import time
-
 import numpy as np
 import timeit
 
-a = np.random.randint(-1000, 1000, 10)
+a = np.random.randint(-100000, 100000, 10000)
 print(a)
 
 #   Пузырьковая сортировка (Bubble Sort);
+def bubble_sort(a):
+    n = len(a)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if a[j] > a[j + 1]:
+                a[j], a[j + 1] = a[j + 1], a[j]
+
+b = []
+b = a.copy()
+
+tt0 = time.time()
+bubble_sort(b)
+tt1 = time.time()
+
+print('Сортировка пузырьком: ', tt1 - tt0)
+print(b)
+
+
+
 #   Сортировка выбором (Selection Sort);
-#   Сортировка вставками (Insertion Sort);
 #   Быстрая сортировка (Quick Sort);
 #   Сортировка слиянием (Merge Sort)
 
-inp = input("Enter number: ")
+#inp = input("Enter number: ")
 
 #a = [-3, 5, 0, -8, 1, 10]
 
+#   Сортировка вставками (Insertion Sort);
 def insert_sort(arr):
     for i in range(1, len(arr)):
         key = arr[i]
@@ -25,14 +43,20 @@ def insert_sort(arr):
             j -= 1
         arr[j + 1] = key
 
+
+b = []
+b = a.copy()
+
 tt0 = time.time()
-insert_sort(a)
+insert_sort(b)
 tt1 = time.time()
 
 print('Сортировка вставками: ', tt1 - tt0)
-print(a)
+print(b)
 # Результат: [-8, -3, 0, 1, 5, 10]
-b = a.sort()
+bb = []
+bb = a.copy()
+bb.sort()
 
-print (b)
+print (b-bb)
 

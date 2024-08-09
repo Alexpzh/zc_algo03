00b610077784b651ee8beff01f35de252aefc56d
Alexpzh <166602027+Alexpzh@users.noreply.github.com>
2024-08-09 14:48:41 +0300

    Add metod insert

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
-a = [-3, 5, 0, -8, 1, 10]
+import time
+
+import numpy as np
+import timeit
+
+a = np.random.randint(-1000, 1000, 10)
+print(a)
+
+#   Пузырьковая сортировка (Bubble Sort);
+#   Сортировка выбором (Selection Sort);
+#   Сортировка вставками (Insertion Sort);
+#   Быстрая сортировка (Quick Sort);
+#   Сортировка слиянием (Merge Sort)
+
+inp = input("Enter number: ")
+
+#a = [-3, 5, 0, -8, 1, 10]
 
 def insert_sort(arr):
     for i in range(1, len(arr)):
@@ -9,6 +25,14 @@ def insert_sort(arr):
             j -= 1
         arr[j + 1] = key
 
+tt0 = time.time()
 insert_sort(a)
+tt1 = time.time()
+
+print('Сортировка вставками: ', tt1 - tt0)
 print(a)
 # Результат: [-8, -3, 0, 1, 5, 10]
+b = a.sort()
+
+print (b)
+
